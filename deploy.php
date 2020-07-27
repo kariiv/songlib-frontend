e01a3ec826b5a1e7ec5f07f7120dc3c47a1840e1
SyrStyle <kariiv@users.noreply.github.com>
2020-07-27 22:59:19 +0100

    t

--- deploy.php
+++ deploy.php
@@ -24,10 +24,10 @@ $allowed_ips = array(
 	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
 	'195.37.139.','193.174.' // VZG
 );
-$allowed = false;
+$allowed = true;
 
 $headers = apache_request_headers();
-
+/*
 if (@$headers["X-Forwarded-For"]) {
     $ips = explode(",",$headers["X-Forwarded-For"]);
     $ip  = $ips[0];
@@ -41,7 +41,7 @@ foreach ($allowed_ips as $allow) {
         break;
     }
 }
-
+*/
 if (!$allowed) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
