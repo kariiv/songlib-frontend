7199ed0cbae265f8e6839370a81e2df97a67885f
SyrStyle <kariiv@users.noreply.github.com>
2020-07-27 22:52:16 +0100

    Auto deploy test

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () =>
     <div className="text-center">
         <div className="error mx-auto" data-text="404">404</div>
-        <p className="lead text-gray-800 mb-5">Song Not Found</p>
+        <p className="lead text-gray-800 mb-5">Song Not Found!</p>
         <p className="text-gray-500 mb-0">It looks like you found no song here...</p>
         <Link to='/'>&larr; Back to Home</Link>
     </div>
